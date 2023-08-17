7ce3037ed03dca9232a0284c7d82f1387b24804c
j2seon <ddj04323@gmail.com>
2023-08-17 15:56:12 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+function Main() {
+    return null;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <BrowserRouter>
+          <Routes>
+            {/* 루트 요청 시 Main으로 이동하도록 설정 */}
+            {/* <Route path="/" element={ <Main />}/> */}
+
+            {/* 인덱스로 설정해두면 위의 설정(루트 요청)과 동일하다. */}
+            <Route index element={ <Main />}/>
+
+          </Routes>
+        </BrowserRouter>
+      </>
   );
 }
 

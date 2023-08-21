aac430bf5a51a2c0d11b1df149f211eb86ac735c
j2seon <ddj04323@gmail.com>
2023-08-21 17:38:41 +0900

    navlist 추가

--- src/components/approval/ApprovalNav.jsx
+++ src/components/approval/ApprovalNav.jsx
@@ -2,6 +2,7 @@ import React, {useContext} from 'react';
 import NavStyle from "../common/Nav.module.css";
 import {MenuContext} from "../../context/MenuContext";
 import {Link} from "react-router-dom";
+import Navlist from "../common/Navlist";
 function ApprovalNav() {
 
     const {menuState, toggleMenu} = useContext(MenuContext);
@@ -10,13 +11,21 @@ function ApprovalNav() {
         <div className={`${menuState ? 'sidemenu': 'close sidemenu'}`}>
             <div className={NavStyle.sideTop}>
                 <h1>전자결재</h1>
-                <a href="/">글쓰기</a>
+                <a href="/">새 결재 작성</a>
             </div>
             <div className={NavStyle.sideList}>
-              <Link to='/approval/mylist'>내 기안문서</Link>
+              <Navlist title="내 결재함" data={approvalLink}/>
             </div>
         </div>
     );
 }
 
 export default ApprovalNav;
+
+const approvalLink = [
+  {text:'기안문서', link:'/approval/mylist'},
+  {text:'참조문서', link:'/approval/reflist'},
+  {text:'임시저장문서', link:'/approval/temp'},
+  {text:'결재대기문서', link:'/approval/approving'},
+  {text:'결재완료문서', link:'/approval/approved'},
+]
--- src/components/approval/ele-component/table/ApprovalTable.jsx
+++ src/components/approval/ele-component/table/ApprovalTable.jsx
@@ -3,8 +3,42 @@ import mainCss from '../../../common/main.css'
 import ApprovalTableCss from './ApprovalTable.module.css';
 import TrData from "./TrData";
 
+function TableHeader(){
+  return(
+    <>
+      <colgroup>
+        <col width="15%"/>
+        <col width="15%"/>
+        <col width="15%"/>
+        <col width="200px"/>
+        <col width="15%"/>
+      </colgroup>
+      <thead>
+      <tr>
+        <th className={ApprovalTableCss.list_no}>문서번호</th>
+        <th>긴급여부</th>
+        <th>결재상태</th>
+        <th className={ApprovalTableCss.t_title}>제목</th>
+        <th>기안날짜</th>
+      </tr>
+      </thead>
+    </>
+  );
+}
+
+
 function ApprovalTable({title, data}) {
 
+  if(data.length < 1){
+    return (
+      <>
+        <table className={ApprovalTableCss.list_approval}>
+          <TableHeader/>
+        </table>
+        <div className={ApprovalTableCss.nolist}>해당문서가 없습니다.</div>
+      </>
+    );
+  }
 
   return (
     <div>
@@ -13,22 +47,7 @@ function ApprovalTable({title, data}) {
       }
       <div>
         <table className={ApprovalTableCss.list_approval}>
-          <colgroup>
-            <col width="15%"/>
-            <col width="15%"/>
-            <col width="15%"/>
-            <col width="200px"/>
-            <col width="15%"/>
-          </colgroup>
-          <thead>
-          <tr>
-            <th className={ApprovalTableCss.list_no}>문서번호</th>
-            <th>긴급여부</th>
-            <th>결재상태</th>
-            <th className={ApprovalTableCss.t_title}>제목</th>
-            <th>기안날짜</th>
-          </tr>
-          </thead>
+          <TableHeader/>
           <tbody>
           {
             data.map((value, index) => <TrData key={index} list={value}/>)

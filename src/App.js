ea8aa0594479efcc5d6de6011ca6a72b0b0b5809
minji <alswl9614@gmail.com>
2023-08-21 16:10:57 +0900

    main merge

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from "./layouts/Layout";
+import Board from "./pages/board/Board";
 import SecheduleCreate from './pages/calendar/ScheduleCreate';
-import Layout from './layouts/Layout';
+import BoardMain from "./pages/board/BoardMain";
 import './components/common/nav.css';
 import './components/common/main.css';
 import './components/common/default.css';
@@ -21,39 +23,30 @@ import Anony from "./pages/board/Anony";
 
 
 
-
-function Main() {
-    return null;
-}
-
 function App() {
   return (
       <>
         <BrowserRouter>
           <Routes>
-            <Route path='/' element={<Layout />}>
-              {/* 루트 요청 시 Main으로 이동하도록 설정 */}
-              {/* <Route path="/" element={ <Main />}/> */}
-
-              {/* 인덱스로 설정해두면 위의 설정(루트 요청)과 동일하다. */}
-              <Route index element={ <Main />} />
-              <Route path="calendar">
-                <Route index element={<SecheduleCreate/>}/>
+              <Route path="/" element={<Layout/>}>
+                  <Route path="calendar">
+                      <Route index element={<SecheduleCreate/>}/>
+                  </Route>
+                  <Route path="/work" element={ <Work /> }/>
+                  <Route path="/work/admin" element={ <WkAdmin /> }/>
+                  <Route path="/work/workside" element={ <WorkSide /> }/>
+                  <Route path="/work/mywork" element={ <MyWork /> }/>
+                  <Route path="/work/mydept" element={ <MyDept />}/>
+                  <Route path="/work/dept" element={ <Dept />}/>
+                  <Route path="/board" element={<BoardMain/>}/>
+                  <Route path="/board/notice" element={ <Notice />}/>
+                  <Route path="/board/report" element={ <Report />}/>
+                  <Route path="/board/menu" element={ <Menu />}/>
+                  <Route path="/board/common" element={ <Common />}/>
+                  <Route path="/board/brddept" element={ <BrdDept />}/>
+                  <Route path="/board/brdadmin" element={ <BrdAdmin />}/>
+                  <Route path="/board/anony" element={ <Anony />}/>
               </Route>
-            </Route>
-            <Route path="/work" element={ <Work /> }/>
-            <Route path="/work/admin" element={ <WkAdmin /> }/>
-            <Route path="/work/workside" element={ <WorkSide /> }/>
-            <Route path="/work/mywork" element={ <MyWork /> }/>
-            <Route path="/work/mydept" element={ <MyDept />}/>
-            <Route path="/work/dept" element={ <Dept />}/>
-            <Route path="/board/notice" element={ <Notice />}/>
-            <Route path="/board/report" element={ <Report />}/>
-            <Route path="/board/menu" element={ <Menu />}/>
-            <Route path="/board/common" element={ <Common />}/>
-            <Route path="/board/brddept" element={ <BrdDept />}/>
-            <Route path="/board/brdadmin" element={ <BrdAdmin />}/>
-            <Route path="/board/anony" element={ <Anony />}/>
           </Routes>
         </BrowserRouter>
       </>
---
+++ src/pages/home/HomeNav.jsx
@@ -0,0 +1,22 @@
+import React, {useContext} from 'react';
+import NavStyle from "../../components/common/Nav.module.css";
+import {MenuContext} from "../../context/MenuContext";
+
+function HomeNav() {
+    const {menuState, toggleMenu} = useContext(MenuContext);
+    return (
+
+
+        <div className={NavStyle.sidemenu }>
+            <div className={NavStyle.sideTop}>
+                <h1>HOME</h1>
+                <a href="/">글쓰기</a>
+            </div>
+            <div className={NavStyle.sideList}>
+
+            </div>
+        </div>
+    );
+}
+
+export default HomeNav;

c1549d76861bc6abc5d454bbeea7219c1a626623
duckdin_ <dbrs96@gmail.com>
2023-08-30 15:03:34 +0900

    메인 툴바 수정

--- src/pages/home/HomeNav.jsx
+++ src/pages/home/HomeNav.jsx
@@ -1,22 +1,18 @@
 import React, {useContext} from 'react';
 import NavStyle from "../../components/common/Nav.module.css";
 import {MenuContext} from "../../context/MenuContext";
+import Main from './Main';
 
 function HomeNav() {
     const {menuState, toggleMenu} = useContext(MenuContext);
     return (
 
 
-        <div className={NavStyle.sidemenu }>
-            <div className={NavStyle.sideTop}>
-                <h1>HOME</h1>
-                <br/>
-                <a href="/" className={NavStyle.new}>글쓰기</a>
-            </div>
-            <div className={NavStyle.sideList}>
-
-            </div>
+        <div className={NavStyle.hideSidemenu }>
+            
         </div>
+        
+
     );
 }
 
--- src/pages/home/Main.js
+++ src/pages/home/Main.js
@@ -3,11 +3,10 @@ import "./main.css"
 function Main() {
 
     return (
-        <>
         <div className="header">
             <h3>메인페이지</h3>
+            <button className="regist">회원 등록</button>
         </div>
-        </>
     )
 }
 

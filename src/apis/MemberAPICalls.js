7721a070484956cb9f62c74d4d7b966761f3fc14
duckdin_ <dbrs96@gmail.com>
2023-09-06 21:56:34 +0900

    fix

--- src/apis/MemberAPICalls.js
+++ src/apis/MemberAPICalls.js
@@ -66,7 +66,6 @@ export const callLogoutAPI = () => {
     return async (dispatch, getState) => {
 
         dispatch({ type: POST_LOGIN, payload: '' });
-        dispatch({ type: POST_LOGIN, payload: '' });
         console.log('[MemberAPICalls] callLogoutAPI RESULT : SUCCESS');
     };
 }

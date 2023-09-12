6a837274adcd2048c58d213a0b751c13c2bd0a25
minji <alswl9614@gmail.com>
2023-09-12 15:10:23 +0900

    예전꺼 commit

--- src/pages/board/PostView.jsx
+++ src/pages/board/PostView.jsx
@@ -1,64 +1,159 @@
 import mainCSS from '../../components/common/main.module.css';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import PostCSS from './PostView.module.css';
 import {LoadingSpiner} from '../../components/common/other/LoadingSpiner'
+import ReactQuill from 'react-quill';
 
 import{
-    callPostViewAPI
+    callPostViewAPI,
+    callPostUpdateAPI
 } from '../../apis/BoardAPICalls'
 
 function PostView() {
 
-    const navigate = useNavigate();
+    const quillRef = useRef(null);
     const dispatch = useDispatch();
     const params = useParams();
     const post  = useSelector(state => state.boardReducer); 
-    const postCode = params.postCode;
 
-    console.log('postCode', postCode);
-    console.log('-------',post);
-    
+    console.log('post',post);
+
+    const [modifyMode, setModifyMode] = useState(false);
+    const navigate = useNavigate();
+
+    const [form, setForm] = useState({});
+
     useEffect(() => {
       if(post.length > 0) return ;
-      console.log('PostView 컴포넌트가 렌더링 됨');
 
-      dispatch(callPostViewAPI({
-          postCode: postCode
-      }))
+          console.log('[PostUpdate] postCode : ', params.postCode);
+
+          dispatch(callPostViewAPI({	
+              postCode: params.postCode
+          }));                     
+      }
+  ,[]);
+    if(post.length < 1) return <LoadingSpiner />
+
+    const onClickPostModifyHandler = () => {
+      setModifyMode(true);
+      setForm({
+        postCode: post.postCode,
+        postTitle: post.postTitle,
+        postDate: post.postDate,
+        postContents: post.postContents,
+        boardCategory: post.boardCategory,
+        boardCode: post.boardCode,
+        memberCode: post.memberCode,
+      });
+    }
+
+    const onChangeHandler = (e) => {
+      setForm({
+          ...form,
+          [e.target.name]: e.target.value
+      });
+  };
+
       
-  }, [dispatch, postCode]);
-  if(post.length < 1) return <LoadingSpiner />
+    const onClickPostUpdate = (postCode) => {
+      console.log(postCode);
+      navigate((0), {replace: false});
+    }
+
+    const titleStyle = {    // 제목 CSS
+      marginLeft: '3%',
+      paddingInline: '20px',
+      marginTop: '40px',
+      fontSize: '23px',
+      fontWeight: '600',
+    };
+
+    const contentsStyle = { // 내용 CSS
+
+    }
+
+    const toolbarOptions = [
+      [{ header: [1, 2, 3, false] }],
+      ["bold", "italic", "underline", "strike"],
+      ["blockquote"],
+      [{ list: "ordered" }, { list: "bullet" }],
+      [{ color: [] }, { background: [] }],
+      [{ align: [] }],
+      ['link', "image", "video"]
+    ]; 
 
     
-    const onClickPostUpdate = () => {
-      console.log(postCode);
-      navigate(`/board/update/${postCode}`, {replace: false});
-    }
 
     return (
 <>
         <div className={mainCSS.maintitle}>
         <h2>{post.boardCategory}</h2>
         </div>
-
-              <div className={ PostCSS.boardtitle }>{post.postTitle}</div>
-              <div className={ PostCSS.actfnt}>작성자 | {post.postDate} | {post.postCode} | 조회수 </div>
-              <div className={ PostCSS.postcont}>{post.postContents}</div>
-           
-                <div className={ PostCSS.boardcontentsline}></div>
         
+        
+              <input
+                  name='postTitle'
+                  placeholder="제목을 입력해주세요."
+                  value={ (!modifyMode ? post.postTitle : form.postTitle) || ''}
+                  onChange={ onChangeHandler }
+                  readOnly={ modifyMode ? false : true }
+                  style={titleStyle}
+                  ></input>
+              <div className={ PostCSS.actfnt}>{post.memberCode} | {post.postDate} | {post.postCode} | 조회수 </div>
+            
+           
+        
+        
+        
+        
+        {modifyMode ? (
+        <div className={PostCSS.quill}>
+          <ReactQuill
+           name='postContents'
+           placeholder="내용을 입력해주세요."
+           ref={quillRef}
+           modules={{
+           toolbar: toolbarOptions
+            
+        }}
+           onChange={(value, delta, source,editor)=>{
+                        setForm((prev)=> ({...prev, postContents:editor.getHTML()}))
+                    }}
+          />
+          <button
+            className={PostCSS.boardupload}
+            onClick={onClickPostUpdate}
+          >
+            수정완료
+          </button>
+        </div>
+      ) : (
+        <div className={PostCSS.postcont}>
+          {post.postContents}
+        </div>
+      )}
+      
+      <div className={ PostCSS.boardcontentsline}></div>
         <div className={ PostCSS.naranhe}>
+        {!modifyMode &&
           <button 
             className={ PostCSS.boardupload} 
-            onClick={onClickPostUpdate}>
+            onClick={onClickPostModifyHandler}>
               수정하기
           </button>
-        <div className={ PostCSS.boarddelete}>삭제하기</div>
+        }
+        {!modifyMode &&
+          <button 
+            className={ PostCSS.boarddelete} 
+            onClick={onClickPostModifyHandler}>
+              삭제하기
+          </button>
+        }
         </div>
-    
-                
+
     </>
     );
 }
